2326266000fadd2effae9e9728e3f18c6059c8c8
Uche Monyei <uchechukwumartin24@gmail.com>
2025-12-08 16:45:54 +0100

    new stuff

--- resources/views/components/recaptcha.blade.php
+++ resources/views/components/recaptcha.blade.php
@@ -15,22 +15,102 @@
 
     @once
         @push('scripts')
-            <script src="https://www.google.com/recaptcha/api.js?render={{ $siteKey }}" async defer></script>
             <script>
                 (function() {
                     const siteKey = @json($siteKey);
+                    const sources = [
+                        `https://www.google.com/recaptcha/api.js?render=${siteKey}`,
+                        `https://www.recaptcha.net/recaptcha/api.js?render=${siteKey}`,
+                    ];
+                    const LOADER_FLAG = '__recaptchaLoading';
+                    const LOAD_TIMEOUT = 10000;
+                    const POLL_INTERVAL = 200;
+                    let recaptchaReadyPromise;
 
-                    function executeRecaptcha(form, action) {
-                        return new Promise((resolve, reject) => {
-                            if (!window.grecaptcha) {
-                                reject(new Error('Google reCAPTCHA not available'));
+                    function injectRecaptchaScript() {
+                        if (window.grecaptcha || window[LOADER_FLAG]) {
+                            return;
+                        }
+
+                        window[LOADER_FLAG] = true;
+                        const queue = sources.slice();
+
+                        function loadNext() {
+                            if (!queue.length) {
+                                window[LOADER_FLAG] = false;
                                 return;
                             }
 
-                            grecaptcha.ready(function() {
-                                grecaptcha.execute(siteKey, { action: action || 'form' })
-                                    .then(resolve)
-                                    .catch(reject);
+                            const src = queue.shift();
+                            const script = document.createElement('script');
+                            script.src = src;
+                            script.async = true;
+                            script.defer = true;
+                            script.onload = function () {
+                                window[LOADER_FLAG] = false;
+                                document.dispatchEvent(new CustomEvent('recaptcha:loaded'));
+                            };
+                            script.onerror = function () {
+                                loadNext();
+                            };
+                            document.head.appendChild(script);
+                        }
+
+                        loadNext();
+                    }
+
+                    function waitForRecaptcha() {
+                        if (window.grecaptcha) {
+                            return Promise.resolve(window.grecaptcha);
+                        }
+
+                        injectRecaptchaScript();
+
+                        if (!recaptchaReadyPromise) {
+                            recaptchaReadyPromise = new Promise((resolve, reject) => {
+                                const timeoutId = setTimeout(() => {
+                                    cleanup();
+                                    reject(new Error('Google reCAPTCHA not available'));
+                                }, LOAD_TIMEOUT);
+
+                                const intervalId = setInterval(() => {
+                                    if (window.grecaptcha) {
+                                        cleanup();
+                                        resolve(window.grecaptcha);
+                                    }
+                                }, POLL_INTERVAL);
+
+                                function cleanup() {
+                                    clearTimeout(timeoutId);
+                                    clearInterval(intervalId);
+                                    document.removeEventListener('recaptcha:loaded', onLoaded);
+                                }
+
+                                function onLoaded() {
+                                    if (window.grecaptcha) {
+                                        cleanup();
+                                        resolve(window.grecaptcha);
+                                    }
+                                }
+
+                                document.addEventListener('recaptcha:loaded', onLoaded, { once: true });
+                            }).catch(error => {
+                                recaptchaReadyPromise = null;
+                                throw error;
+                            });
+                        }
+
+                        return recaptchaReadyPromise;
+                    }
+
+                    function executeRecaptcha(form, action) {
+                        return waitForRecaptcha().then(() => {
+                            return new Promise((resolve, reject) => {
+                                grecaptcha.ready(function() {
+                                    grecaptcha.execute(siteKey, { action: action || 'form' })
+                                        .then(resolve)
+                                        .catch(reject);
+                                });
                             });
                         });
                     }
@@ -63,6 +143,8 @@
                                 alert('Captcha validation failed. Please reload and try again.');
                             });
                     }, true);
+
+                    injectRecaptchaScript();
                 })();
             </script>
         @endpush
